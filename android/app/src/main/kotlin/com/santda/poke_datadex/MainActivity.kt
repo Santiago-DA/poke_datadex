878b5e37578d3c04b09c5fdd529c50ab861d6453
Santiago-DA <sa.aristimuno@correo.unimet.edu.ve>
2025-03-25 16:45:36 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/santda/poke_datadex/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.santda.poke_datadex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter App',
+        theme: ThemeData(primarySwatch: Colors.blue), // Default theme
+        home: Container(
+          child: Text("Something"),
+        ));
+  }
+}
